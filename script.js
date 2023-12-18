e83e04836d44d77c988e21e170af40ec02baa142
deerrppp <deerrppp@gmail.com>
2023-12-18 22:28:24 +0800

    Determines whether or not a given year is a leap year.
     Leap years are determined by the following rules:
    > Leap years are years divisible by four (like 1984 and 2004). However, years divisible by 100 are not leap years (such as 1800 and 1900) unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years). (Yes, it's all pretty confusing)
    >
    > -- <cite>[Learn to Program](https://pine.fm/LearnToProgram/chap_06.html) by Chris Pine</cite>

--- script.js
+++ script.js
@@ -381,16 +381,29 @@
 // console.log(reverseString("aaba"));
 
 //SUM OF ALL NUMBERS
-const sumAll = function (num1, num2) {
-  if (num1 < 0 || num2 < 0) return "ERROR";
-  // if (typeof num1 !== "number" || typeof num2 !== "number") return "ERROR";
-  if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
-  if (num1 > num2) [num1, num2] = [num2, num1];
-  let total = 0;
-  for (let i = num1; i <= num2; i++) {
-    total += i;
-  }
-  return total;
+// const sumAll = function (num1, num2) {
+//   if (num1 < 0 || num2 < 0) return "ERROR";
+//   // if (typeof num1 !== "number" || typeof num2 !== "number") return "ERROR";
+//   if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+//   if (num1 > num2) [num1, num2] = [num2, num1];
+//   let total = 0;
+//   for (let i = num1; i <= num2; i++) {
+//     total += i;
+//   }
+//   return total;
+// };
+// let addition = sumAll(7, 3);
+// console.log(addition);
+
+//LEAPYEAR
+//## Hints
+//- use an `if` statement and `&&` to make sure all the conditions are met properly
+//Leap years are years divisible by four (like 1984 and 2004).
+//However, years divisible by 100 are not leap years (such as 1800 and 1900)
+//unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years).
+//(Yes, it's all pretty confusing)
+const leapYears = function (num) {
+  return num % 4 === 0 && (num % 100 !== 0 || num % 400 === 0);
 };
-let addition = sumAll(7, 3);
-console.log(addition);
+let test = leapYears(1800);
+console.log(test);
